5ab96616abc24b53b5c4843b41ed951d8d529365
Sumit Mishra <sm0141136@gmail.com>
2024-08-23 22:37:10 +0530

    try

--- ARRAYLIST/SwapTwoNumber.java
+++ ARRAYLIST/SwapTwoNumber.java
@@ -8,7 +8,7 @@ public class SwapTwoNumber {
     }
     public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
-        list.add(2);
+        list.add(3);
         list.add(4);
         list.add(9);
         list.add(3);
